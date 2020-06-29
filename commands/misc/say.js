7efc5646ef027840bdb5b9943042156b7ccede11
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-29 20:56:17 +0200

    Update say.js

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -2,20 +2,20 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "say",
     category: "misc",
-    description: "Een nieuwe update melden! (ADMINISTRATOR VEREIST)",
+    description: "Een nieuw bericht melden! (ADMINISTRATOR VEREIST)",
     usage: "<msg>",
     run: async (bot, message, args) => {
       if (!message.member.permissions.has("ADMINISTRATOR")) return;
       let MSG = message.content.split(`${bot.prefix}say `).join("");
       if (!MSG)
-        return message.channel.send(`Geen update opgegeven!`);
+        return message.channel.send(`Geen bericht opgegeven!`);
         const Embed = new MessageEmbed()
         .setTitle('Nieuwe Update')
         .setDescription(MSG)
         .setColor('RANDOM')
         .setTimestamp()
         .setFooter(`© daan2341, 2020 - 2021`)
-        message.channel.send(Embed)
+        message.channel.send(Embed);
       message.delete();
     },
   };
