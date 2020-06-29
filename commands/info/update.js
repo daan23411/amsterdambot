0edf44241e082f07d07f320568e6e02e3fbb76bb
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-29 20:46:34 +0200

    Update update.js

--- commands/info/update.js
+++ commands/info/update.js
@@ -5,12 +5,11 @@ module.exports={
     description: "Laat de bot een update uitvoeren",
     usage: "<msg>",
     run: async(bot,message,args)=>{
-        let MSG = message.content.split(`${bot.prefix}say `).join(" ")
+        let MSG = message.content.split(`${bot.prefix}say `).join("")
         if(!MSG)return message.reply('Je hebt geen bericht opgegeven')
         const Embed = new MessageEmbed()
         .setTitle('Nieuw Bericht')
         .setDescription(MSG)
-        .setThumbnail(message.author.displayAvatarURL())
         .setColor('RANDOM')
         .setTimestamp()
         .setFooter(`© daan2341, 2020 - 2021`)
